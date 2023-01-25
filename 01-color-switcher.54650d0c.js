c802868d4e580617d150880fc91402fea26974c5
Sandrixman <75435132+Sandrixman@users.noreply.github.com>
2023-01-25 17:44:10 +0000

    Deploying to gh-pages from @ Sandrixman/goit-js-hw-09@ce14f38e48f968e8060630878aeac317ecde9537 🚀

---
+++ 01-color-switcher.54650d0c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.setAttribute("disabled","");let d=null;function r(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(()=>{d=setInterval(r,1e3),t.setAttribute("disabled",""),e.removeAttribute("disabled","")})),e.addEventListener("click",(()=>{clearInterval(d),t.removeAttribute("disabled",""),e.setAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.54650d0c.js.map
